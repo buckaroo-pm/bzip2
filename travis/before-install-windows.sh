84b29cc20d34354d11d2825d25d53f5a98d73dc7
njlr <njlr@users.noreply.github.com>
2018-12-03 23:01:23 +0000

    * Travis tweaks

--- travis/before-install-windows.sh
+++ travis/before-install-windows.sh
@@ -3,6 +3,7 @@ choco install buck
 
 powershell refreshenv
 
+powershell "Set-PSRepository -Name PSGallery -InstallationPolicy Trusted"
 powershell "Install-Module –Name PowerShellGet"
 powershell "Install-Module -Name PSCX -AllowClobber"
 powershell "Install-Module -Name VSSetup -AllowClobber"
