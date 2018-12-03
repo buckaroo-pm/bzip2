77979f353426f1609aa06060edf6049db5950141
njlr <njlr@users.noreply.github.com>
2018-12-03 21:52:04 +0000

    * Adds bash profile export to Windows Travis

--- travis/before-install-windows.sh
+++ travis/before-install-windows.sh
@@ -2,5 +2,11 @@
 choco install buck
 export PATH=$PATH:"/c/Program Files/Java/jdk1.8.0_191/bin"
 export JAVA_HOME="/c/Program Files/Java/jdk1.8.0_191"
+
+touch ~/.bash_profile
+echo -e "\nJAVA_HOME=$JAVA_HOME\n" >> ~/.bash_profile
+
+cat ~/.bash_profile
+
 java -version
 buck --version
