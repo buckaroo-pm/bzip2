c5815f2bd31ce47cbcca01f67d391fc58299bc10
njlr <njlr@users.noreply.github.com>
2018-12-03 22:37:45 +0000

    * Init powershell vars on Travis Windows

--- travis/before-install-windows.sh
+++ travis/before-install-windows.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 choco install buck
 
+powershell refreshenv
+
+powershell "Install-Module -Name PSCX -AllowClobber"
+powershell "Install-Module -Name VSSetup -AllowClobber"
+powershell "Import-VisualStudioVars 2017 amd64"
+
+powershell refreshenv
+
 export PATH=$PATH:"/c/Program Files/Java/jdk1.8.0_191/bin"
 export JAVA_HOME="/c/Program Files/Java/jdk1.8.0_191"
 
